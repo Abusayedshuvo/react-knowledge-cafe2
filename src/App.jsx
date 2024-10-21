e4c7cb317bec5a284c0cec39a16b4102b1db3fd5
Abusayedshuvo <abusayedshuvo24@gmail.com>
2024-10-21 19:18:10 +0600

    react js react-knowledge-cafe2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor,
+        commodi?
+      </h1>
+    </>
+  );
+}
+
+export default App;
